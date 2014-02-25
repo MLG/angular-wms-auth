2421a96fbf23bf39d4aa70f9ab089003047b3099
Eugene OZ <normandiggs@gmail.com>
2014-02-25 14:54:29 +0400

    init

---
+++ service.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('mlgWmsAuth', [])
+  .service('Auth', function Auth($http) {
+    var Model = {
+      authenticate: function (init_path, auth_path) {
+        $http.get(init_path)
+          .success(function (response) {
+            if (!angular.isUndefined(response.session_user_name)) {
+              Model.session = {user_name: response.session_user_name};
+              Model.is_authenticated = true;
+              Model.employee_id = response.employee_id
+            }
+          })
+          .error(function (data, status, headers) {
+            if (status == 0 || (status > 299 && status < 500)) {
+              window.location.href = (headers('Location') || auth_path);
+              return false;
+            }
+          });
+      },
+      session: {},
+      employee_id: null,
+      is_authenticated: false
+    };
+    return Model;
+  });
