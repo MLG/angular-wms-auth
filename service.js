a6c27c0576e04d25caee8db12c1823955e21c1d7
Eugene OZ <normandiggs@gmail.com>
2014-03-03 12:01:10 +0400

    0.0.2

--- service.js
+++ service.js
@@ -13,9 +13,8 @@ angular.module('mlgWmsAuth', [])
             }
           })
           .error(function (data, status, headers) {
-            if (status == 0 || (status > 299 && status < 500)) {
+            if (status === 0 || (status > 299 && status < 500)) {
               window.location.href = (headers('Location') || auth_path);
-              return false;
             }
           });
       },
@@ -24,4 +23,16 @@ angular.module('mlgWmsAuth', [])
       is_authenticated: false
     };
     return Model;
+  })
+  .directive('wmsAuthRequest', function () {
+    return {
+      restrict: 'AEC',
+      scope:{
+        authPath: '@',
+        initPath: '@'
+      },
+      controller: function ($scope, Auth) {
+        Auth.authenticate($scope.initPath, $scope.authPath);
+      }
+    }
   });
